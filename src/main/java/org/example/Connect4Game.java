b0a670811a90ed39d360fa691bc7c3c1873e936f
Keyurx11 <58322871+Keyurx11@users.noreply.github.com>
2023-01-10 23:06:56 +0000

    Printing game board

--- src/main/java/org/example/Connect4Game.java
+++ src/main/java/org/example/Connect4Game.java
@@ -2,16 +2,32 @@ package org.example;
 
 public class Connect4Game {
 
+    // Constants for the game board
+    public static final int ROWS = 6;
+    public static final int COLUMNS = 7;
+    public static char[][] board = new char[ROWS][COLUMNS];
+
     public static void main(String[] args) {
         // Create the game board
-        char[][] board = new char[6][7];
-        for (int row = 0; row < 6; row++) {
-            for (int col = 0; col < 7; col++) {
-                board[row][col] = '*';
-                System.out.print(board[row][col]);
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLUMNS; col++) {
+                board[row][col] = ' ';
             }
-            System.out.println();
         }
+        printBoard();
     }
 
+    private static void printBoard() {
+        // Print the game board
+        for (int row = 0; row < ROWS; row++) {
+            System.out.print("| ");
+            for (int col = 0; col < COLUMNS; col++) {
+                System.out.print(board[row][col] + " | ");
+            }
+            System.out.println();
+
+        }
+        System.out.println("-----------------------------");
+        System.out.println("| 1 | 2 | 3 | 4 | 5 | 6 | 7 |");
+    }
 }
