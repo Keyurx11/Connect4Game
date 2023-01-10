94f0efe064c61d405e4c228a69823be49ae63f87
Keyurx11 <58322871+Keyurx11@users.noreply.github.com>
2023-01-10 23:45:20 +0000

    Placing tokens on board for player 1

--- src/main/java/org/example/Connect4Game.java
+++ src/main/java/org/example/Connect4Game.java
@@ -1,20 +1,46 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Connect4Game {
 
     // Constants for the game board
     public static final int ROWS = 6;
     public static final int COLUMNS = 7;
+    public static final char PLAYER_1_TOKEN = 'X';
+    public static final char PLAYER_2_TOKEN = 'O';
+    private static final char EMPTY_SPACE = ' ';
     public static char[][] board = new char[ROWS][COLUMNS];
 
     public static void main(String[] args) {
         // Create the game board
         for (int row = 0; row < ROWS; row++) {
             for (int col = 0; col < COLUMNS; col++) {
-                board[row][col] = ' ';
+                board[row][col] = EMPTY_SPACE;
             }
         }
-        printBoard();
+        //Reading player input
+        Scanner scanner = new Scanner(System.in);
+
+        // Initialize the current player
+        char currentPlayer = PLAYER_1_TOKEN;
+
+        // Main game loop
+        while (true) {
+            printBoard();
+
+            // Prompt the player for their next move
+            System.out.print("Player " + currentPlayer + ", enter column number: ");
+            int col = scanner.nextInt();
+
+            // Place the token in the specified column
+            for (int row = ROWS - 1; row >= 0; row--) {
+                if (board[row][col] == EMPTY_SPACE) {
+                    board[row][col] = currentPlayer;
+                    break;
+                }
+            }
+        }
     }
 
     private static void printBoard() {
@@ -30,4 +56,5 @@ public class Connect4Game {
         System.out.println("-----------------------------");
         System.out.println("| 1 | 2 | 3 | 4 | 5 | 6 | 7 |");
     }
+
 }
