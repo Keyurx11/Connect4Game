36ef35f3fbc3f22525bbc5a906abfe51caa403c9
Keyurx11 <58322871+Keyurx11@users.noreply.github.com>
2023-01-14 08:35:56 +0000

    Implemented tests for checkForDraw and checkIfValidCol functions.

--- src/test/java/org/example/Connect4GameTest.java
+++ src/test/java/org/example/Connect4GameTest.java
@@ -29,4 +29,49 @@ public class Connect4GameTest {
         }
         assertFalse(Connect4Game.checkIfValidCol(1));
     }
+
+    @Test
+    public void testPlaceToken() {
+        // Initialize the game board
+        for (int row = 0; row < Connect4Game.ROWS; row++) {
+            for (int col = 0; col < Connect4Game.COLUMNS; col++) {
+                Connect4Game.board[row][col] = Connect4Game.EMPTY_SPACE;
+            }
+        }
+
+        // Test placing a token in a valid column
+        assertTrue(Connect4Game.placeToken(0, Connect4Game.PLAYER_1_TOKEN));
+        assertEquals(Connect4Game.board[5][0], Connect4Game.PLAYER_1_TOKEN);
+
+        // Test placing a token in a full column
+        for (int row = 0; row < Connect4Game.ROWS; row++) {
+            Connect4Game.board[row][1] = Connect4Game.PLAYER_1_TOKEN;
+        }
+        assertFalse(Connect4Game.placeToken(1, Connect4Game.PLAYER_1_TOKEN));
+
+        // Test placing a token in an invalid column
+        assertFalse(Connect4Game.placeToken(-1, Connect4Game.PLAYER_1_TOKEN));
+        assertFalse(Connect4Game.placeToken(Connect4Game.COLUMNS, Connect4Game.PLAYER_1_TOKEN));
+    }
+
+    //TODO Test for checkForWin
+    @Test
+    public void testCheckForDraw() {
+// Initialize the game board
+        for (int row = 0; row < Connect4Game.ROWS; row++) {
+            for (int col = 0; col < Connect4Game.COLUMNS; col++) {
+                Connect4Game.board[row][col] = Connect4Game.PLAYER_1_TOKEN;
+            }
+        }    // Test for a draw with a full board
+        assertTrue(Connect4Game.checkForDraw());
+
+        // Test for a draw with an empty space on the board
+        Connect4Game.board[0][0] = Connect4Game.EMPTY_SPACE;
+        assertFalse(Connect4Game.checkForDraw());
+    }
+
+    //TODO Test for clearColumn
+
+    //TODO Test for clearSurrounding
+
 }
